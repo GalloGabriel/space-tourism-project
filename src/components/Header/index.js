82a2a8347fedcdaa10d8b3a0f9b5543b3be9b8d4
GalloGabriel <gabriel.gallo2242@gmail.com>
2022-01-05 16:46:21 -0300

    fixing home page code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -38,23 +38,7 @@ export default function Header(){
               
             })}
           </ul>
-        </div>
-      
-      
-        {homeData.map((data, index)=>{
-          return(
-            <div key={index} className="info">
-              <span>{data.subtitle}</span>
-              <h1>{data.title}</h1>
-              <p>{data.text}</p>
-            </div>    
-            
-          );
-        })}
-
-        <div className="circle">
-          <span>Explore</span>
-        </div>      
+        </div>         
       
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,26 @@
 import Header from '../../components/Header';
+import homeData from '../../data/home.json';
 import './home.css';
 
 export default function Home(){
   return(
     <div className="bg-home">
       <Header />
+
+      {homeData.map((data, index)=>{
+          return(
+            <div key={index} className="info">
+              <span>{data.subtitle}</span>
+              <h1>{data.title}</h1>
+              <p>{data.text}</p>
+            </div>    
+            
+          );
+        })}
+
+        <div className="circle">
+          <span>Explore</span>
+        </div>    
     </div>
   );
 }
